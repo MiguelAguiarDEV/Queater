f155c111f422c5fe8898b92714e5ebdd182e941a
Miguel Alejandro Santiesteban Aguiar <miguel.santiesteban.aguiar@gmail.com>
2025-06-01 03:16:12 +0200

    feat: add Cloudflare tunnel service to Docker Compose and update environment configuration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Routing\UrlGenerator;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
@@ -18,8 +20,12 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot(UrlGenerator $url): void
     {
         Vite::prefetch(concurrency: 3);
+
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
